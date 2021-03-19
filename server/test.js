ca263abb1db77ca5ffadd4a4cbb7c6e79bbd9309
jichangee <jichangee@gmail.com>
2021-03-19 13:42:42 +0800

    ~

--- server/test.js
+++ server/test.js
@@ -1,9 +1,11 @@
 const request = require('request')
 
-request.get('https://www.baidu.com', (err, res, body) => {
-  if (err) {
-    console.log('err', err)
-    return
-  }
-  console.log('body', body);
-})
+// request.get('https://www.baidu.com', (err, res, body) => {
+//   if (err) {
+//     console.log('err', err)
+//     return
+//   }
+//   console.log('body', body);
+// })
+
+request.get('https://sctapi.ftqq.com/SCT18829Twar7gK7U0jMefYqIKPfBtwbQ.send?title=' + encodeURIComponent('外星剧名'))
--- server/utils/serverpush.js
+++ server/utils/serverpush.js
@@ -1,11 +1,11 @@
 /**
  * Server酱推送
  */
-const https = require('https')
+const request = require('request')
 
 const sendByServer = (title) => {
   return new Promise((resolve) => {
-    https.get('https://sctapi.ftqq.com/SCT18829Twar7gK7U0jMefYqIKPfBtwbQ.send?title=' + encodeURIComponent(title), function () {
+    request.get('https://sctapi.ftqq.com/SCT18829Twar7gK7U0jMefYqIKPfBtwbQ.send?title=' + encodeURIComponent(title), function () {
       resolve()
     })
   })
